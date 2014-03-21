29b3b85db57a1e0cb47f76b0f7483a5332eeaa52
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-03-21 16:53:48 +0100

    First commit

---
+++ Assets/Code FrameWork/Utility/DebugHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class DebugHelper 
+{
+
+    public static void LogList<T>(List<T> list)
+    {
+        int i = 0;
+        foreach (T t in list)
+        {
+            Debug.Log(i + "st in list: " + t.ToString());
+            i++;
+        }
+    }
+}
