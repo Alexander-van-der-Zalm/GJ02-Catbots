c7254ce3d368b53223da2bc91923892f95ba8234
Alexander van der Zalm <alexandervdzalm@gmail.com>
2014-03-22 22:26:36 +0100

    Before changing sprite pivots

--- Assets/Code Game Specific/Components/Block.cs
+++ Assets/Code Game Specific/Components/Block.cs
@@ -15,4 +15,14 @@ public class Block : MonoBehaviour
     {
 	    
 	}
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag != "Player")
+            return;
+
+        float force1 = (rigidbody2D.velocity * rigidbody2D.mass).magnitude;
+        float force2 = (other.rigidbody.velocity * rigidbody2D.mass).magnitude;
+        Debug.Log(gameObject.tag + " " + force1 + " " + other.gameObject.tag +  " " + force2);
+    }
 }
